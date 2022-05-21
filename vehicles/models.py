6abb42068f37f6dd30bfe785d8a21c47d92a8afe
Patrick Vilchez <pvilchez794@gmail.com>
2022-05-21 18:21:16 -0500

    Validator add to models

--- vehicles/models.py
+++ vehicles/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
-# dd-mm-yyyy hh:mm:ss format in date fields
+
 class Vehicle(models.Model):
-    name = models.CharField(max_length=10, blank=False)
-    alias = models.CharField(max_length=10, blank=False)
+    name = models.CharField(max_length=10, blank=False, validators=[RegexValidator(regex='^[a-zA-Z0-9-_]+$')])
+    alias = models.CharField(max_length=10, blank=False, validators=[RegexValidator(regex='^[a-zA-Z0-9-_]+$')])
     creation_date = models.DateTimeField(auto_now_add=True)
     edit_date = models.DateTimeField(auto_now=True)
     company = models.CharField(max_length=100, blank=False)
